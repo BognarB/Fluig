b69e21b8d66f42c47cd2fc38e29e558d54420cc6
Renato Grael Teixeira <renato.grael@gmail.com>
2016-03-03 11:29:37 -0300

    Correcao depois da cagad@

--- POC-Fluig/components/contactsListView/index.js
+++ POC-Fluig/components/contactsListView/index.js
@@ -47,7 +47,7 @@ app.contactsListView = kendo.observable({
             var item = e.view.params.uid,
             dataSource = contactsListViewModel.get('dataSource'),
             itemModel = dataSource.getByUid(item);
-            itemModel.picture.mediumUrl = processImage(itemModel.picture.thumbnail);
+            itemModel.picture.mediumUrl = processImage(itemModel.picture.large);
 
             if (!itemModel.name.first) {
                 itemModel.name.first = String.fromCharCode(160);
