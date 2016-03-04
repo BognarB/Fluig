86cd338a138d5045fb81abd5110261568bd25c95
Mauricio Kunii <mtakeshi@progress.com>
2016-03-04 14:06:01 +0000

    Changes made by Mauricio Kunii (mtakeshi@progress.com).

--- POC-Fluig/components/home/index.js
+++ POC-Fluig/components/home/index.js
@@ -17,123 +17,127 @@ app.home = kendo.observable({
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function (parent) {
     var provider = app.data.pocFluig,
-    mode = 'signin',
-    registerRedirect = 'novoContatoView',
-    signinRedirect = 'novoContatoView',
-    init = function (error) {
-        if (error) {
-            if (error.message) {
-                alert(error.message);
+        mode = 'signin',
+        registerRedirect = 'novoContatoView',
+        signinRedirect = 'novoContatoView',
+        init = function (error) {
+            if (error) {
+                if (error.message) {
+                    console.log(error);
+                    alert(error.message);
+                }
             }
-        }
 
-        if (app.user) {
-            provider.Users.logout();
-        }
+            if (app.user) {
+                provider.Users.logout();
+            }
 
-        var activeView = mode === 'signin' ? '.signin-view' : '.signup-view';
-
-        if (provider.setup && provider.setup.offlineStorage && !app.isOnline()) {
-            $('.offline').show().siblings().hide();
-        } else {
+            var activeView = mode === 'signin' ? '.signin-view' : '.signup-view';
             app.mobileApp.hideLoading();
-            $(activeView).show().siblings().hide();
-        }
-    },
-    successHandler = function (data) {
-
-        var redirect = mode === 'signin' ? signinRedirect : registerRedirect;
-
-        if (data && data.result) {
-            app.user = data.result;
-            setTimeout(function () {
-                app.mobileApp.navigate('components/' + redirect + '/view.html');
-            }, 0);
-        } else {
-            init();
-        }
-    },
-    homeModel = kendo.observable({
-        displayName: '',
-        email: '',
-        password: '',
-        validateData: function (data) {
-            if (!data.email) {
-                alert('Missing email');
-                return false;
+            if (provider.setup && provider.setup.offlineStorage && !app.isOnline()) {
+                $('.offline').show().siblings().hide();
+            } else {
+                $(activeView).show().siblings().hide();
             }
-
-            if (!data.password) {
-                alert('Missing password');
-                return false;
-            }
-
-            return true;
         },
-        signin: function () {
-            var model = homeModel,
-            email = model.email.toLowerCase(),
-            password = model.password;
+        successHandler = function (data) {
 
-            if (!model.validateData(model)) {
-                return false;
+            var redirect = mode === 'signin' ? signinRedirect : registerRedirect;
+
+            if (data && data.result) {
+                app.user = data.result;
+                setTimeout(function () {
+                    app.mobileApp.navigate('components/' + redirect + '/view.html');
+                }, 0);
+            } else {
+                init();
             }
-            app.mobileApp.showLoading();
-            
-            provider.Users.login(email, password, successHandler, init);
         },
-        register: function () {
-            var model = homeModel,
-            email = model.email.toLowerCase(),
-            password = model.password,
-            displayName = model.displayName,
-            attrs = {
-                Email: email,
-                DisplayName: displayName
-            };
+        homeModel = kendo.observable({
+            displayName: '',
+            userName: '',
+            email: '',
+            password: '',
+            validateData: function (data) {
+                if (!data.userName) {
+                    alert('Missing email');
+                    return false;
+                }
 
-            if (!model.validateData(model)) {
-                return false;
-            }
-            app.mobileApp.showLoading();
-            
-            provider.Users.register(email, password, attrs, successHandler, init);
-        },
-        facebookLogin: function () {
+                if (!data.password) {
+                    alert('Missing password');
+                    return false;
+                }
 
-            var facebookConfig = {
-                name: 'Facebook',
-                loginMethodName: 'loginWithFacebook',
-                endpoint: 'https://www.facebook.com/dialog/oauth',
-                response_type: 'token',
-                client_id: 1086764318010540,
-                redirect_uri: "http://www.facebook.com/connect/login_success.html",
-                access_type: 'online',
-                scope: 'email',
-                display: 'touch'
-            };
+                return true;
+            },
+            signin: function () {
+                var model = homeModel,
+                    userName = model.user.toLowerCase(),
+                    password = model.password;
 
-            var facebook = new IdentityProvider(facebookConfig);
-            app.mobileApp.showLoading();
-            facebook.getAccessToken(function (token) {
-                provider.Users.loginWithFacebook(token)
-                .then(function () {
-                    app.mobileApp.hideLoading();
-                    console.log(facebook.getCurrentUser());
-                    provider.Users.currentUser(successHandler,init);
+                if (!model.validateData(model)) {
+                    return false;
+                }
+                app.mobileApp.showLoading();
+
+                provider.Users.login(userName, password, successHandler, init);
+            },
+            register: function () {
+                var model = homeModel,
+                    email = model.email.toLowerCase(),
+                    userName = model.userName.toLowerCase(),
+                    password = model.password,
+                    displayName = model.displayName,
+                    attrs = {
+                        User: userName,
+                        Email: email,
+                        DisplayName: displayName
+                    };
+                console.log(email,userName,password,attrs);
+
+                if (!model.validateData(model)) {
+                    return false;
+                }
+                app.mobileApp.showLoading();
+
+                provider.Users.register(userName, password, attrs, successHandler, init);
+            },
+            facebookLogin: function () {
+
+                var facebookConfig = {
+                    name: 'Facebook',
+                    loginMethodName: 'loginWithFacebook',
+                    endpoint: 'https://www.facebook.com/dialog/oauth',
+                    response_type: 'token',
+                    client_id: 1086764318010540,
+                    redirect_uri: "http://www.facebook.com/connect/login_success.html",
+                    access_type: 'online',
+                    scope: 'email',
+                    display: 'touch'
+                };
+
+                var facebook = new IdentityProvider(facebookConfig);
+                app.mobileApp.showLoading();
+                facebook.getAccessToken(function (token) {
+                    provider.Users.loginWithFacebook(token)
+                        .then(function () {
+                            app.mobileApp.hideLoading();
+                            console.log(facebook.getCurrentUser());
+                            provider.Users.currentUser(successHandler, init);
+                        });
                 });
-            });
-        },
-        toggleView: function () {
-            mode = mode === 'signin' ? 'register' : 'signin';
-            init();
-        },
-        resetForm: function () {
-            this.set("email", "");
-            this.set("password", "");
-            init();
-        }
-    });
+            },
+            toggleView: function () {
+                mode = mode === 'signin' ? 'register' : 'signin';
+                init();
+            },
+            resetForm: function () {
+                this.set("email", "");
+                this.set("password", "");
+                init();
+            }
+        });
 
     parent.set('homeModel', homeModel);
     parent.set('afterShow', function () {
