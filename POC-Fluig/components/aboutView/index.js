5488d8e1ba39208dbf771107f3f50afaf618efa1
Renato Bognar <rbognar@progress.com>
2016-03-07 12:36:51 +0000

    Changes made by Renato Bognar (rbognar@progress.com).

--- POC-Fluig/components/aboutView/index.js
+++ POC-Fluig/components/aboutView/index.js
@@ -7,10 +7,6 @@ app.aboutView = kendo.observable({
     }
 });
 
-// START_CUSTOM_CODE_aboutView
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_aboutView
 (function(parent) {
     var aboutViewModel = kendo.observable({
         openLink: function(url) {
@@ -28,8 +24,3 @@ app.aboutView = kendo.observable({
 
     parent.set('aboutViewModel', aboutViewModel);
 })(app.aboutView);
-
-// START_CUSTOM_CODE_aboutViewModel
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_aboutViewModel
